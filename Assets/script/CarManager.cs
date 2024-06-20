e92f116304ef4b883de412b5fe724d2d3ad5a935
Munziu <annkanyanga@gmail.com>
2024-06-20 13:22:00 +0300

    Add speedometer, engine audio

--- Assets/script/CarManager.cs
+++ Assets/script/CarManager.cs
@@ -1,6 +1,16 @@
+using SimpleInputNamespace;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using TMPro;
 using UnityEngine;
+public enum GearState
+{
+    Neutral,
+    Running,
+    CheckingChange,
+    Changing
+};
 
 public class CarManager : MonoBehaviour
 {
@@ -15,22 +25,76 @@ public class CarManager : MonoBehaviour
 
     //Rigid body variable
     public Rigidbody RB;
+    public float speed;
+    
 
     //Car control Inputs (Fuel, Steering)
     public float FuelInput, SteeringInput, BrakeInput;
+    public int isEngineRunning;
 
     //Motor Inputs
     public float MotorPower, SteeringPower, BrakePower;
+    public float RPM;
+    public float redLine;
+    public float idleRPM;
+    public TMP_Text rpmText;
+    public TMP_Text gearText;
+    public Transform rpmNeedle;
+    public int currentGear;
+    public float minNeedleRotation;
+    public float maxNeedleRotation;
 
+    public float[] gearRatios;
+    public float differentialRatio;
+    private float currentTorque;
+    private float clutch;
+    private float wheelRPM;
+    public AnimationCurve hpToRPMCurve;
+    private GearState gearState;
+    public float increaseGearRPM;
+    public float decreaseGearRPM;
+    public float changeGearTime = 0.5f;
+
+    public GameObject tireTrail;
+   
     // Start is called before the first frame update
     void Start()
     {
         RB.centerOfMass = CenterOfMass.transform.localPosition;
+        InitiateParticles();
+        
+    }
+
+    void InitiateParticles()
+    {
+        //if (smokePrefab)
+        //{
+        //    wheelParticles.FRWheel = Instantiate(smokePrefab, FRWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, FRWheelCollider.transform)
+        //        .GetComponent<ParticleSystem>();
+        //    wheelParticles.FLWheel = Instantiate(smokePrefab, FLWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, FLWheelCollider.transform)
+        //        .GetComponent<ParticleSystem>();
+        //    wheelParticles.RRWheel = Instantiate(smokePrefab, RRWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, RRWheelCollider.transform)
+        //        .GetComponent<ParticleSystem>();
+        //    wheelParticles.RLWheel = Instantiate(smokePrefab, RLWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, RLWheelCollider.transform)
+        //        .GetComponent<ParticleSystem>();
+        //}
+        if (tireTrail)
+        {
+            Instantiate(tireTrail, FRWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, FRWheelCollider.transform);
+            Instantiate(tireTrail, FLWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, FLWheelCollider.transform);
+            Instantiate(tireTrail, RRWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, RRWheelCollider.transform);
+            Instantiate(tireTrail, RLWheelCollider.transform.position - Vector3.up * FRWheelCollider.radius, Quaternion.identity, RLWheelCollider.transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        rpmNeedle.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(minNeedleRotation, maxNeedleRotation, RPM / (redLine * 1.1f)));
+        rpmText.text = RPM.ToString("0,000") + "rpm";
+        gearText.text = (gearState == GearState.Neutral) ? "N" : (currentGear + 1).ToString();
+        speed = RRWheelCollider.rpm * RRWheelCollider.radius * 2f * Mathf.PI / 10f;
+
         CheckInputs();
         ApplyMotor();
         ApplySteering();  
@@ -42,18 +106,128 @@ public class CarManager : MonoBehaviour
         SteeringInput = SimpleInput.GetAxis("Horizontal");
 
         float MoveDir = Vector3.Dot(transform.forward, RB.velocity);
+        if (gearState != GearState.Changing)
+        {
+            if (gearState == GearState.Neutral)
+            {
+                clutch = 0;
+                if (Mathf.Abs(FuelInput) > 0) gearState = GearState.Running;
+            }
+            else
+            {
+                clutch = Input.GetKey(KeyCode.LeftShift) ? 0 : Mathf.Lerp(clutch, 1, Time.deltaTime);
+            }
+        }
+        else
+        {
+            clutch = 0;
+        }
+        //if (movingDirection < -0.5f && FuelInput > 0)
+        //{
+        //    brakeInput = Mathf.Abs(FuelInput);
+        //}
+        //else if (movingDirection > 0.5f && FuelInput < 0)
+        //{
+        //    brakeInput = Mathf.Abs(FuelInput);
+        //}
+        //else
+        //{
+        //    brakeInput = 0;
+        //}
     }
 
     //Motor Method
     void ApplyMotor(){
-        RLWheelCollider.motorTorque = FuelInput * MotorPower;
-        RRWheelCollider.motorTorque = FuelInput * MotorPower;
+        currentTorque = CalculateTorque();
+        RLWheelCollider.motorTorque = FuelInput * currentTorque;
+        RRWheelCollider.motorTorque = FuelInput * currentTorque;
+    }
+
+    float CalculateTorque()
+    {
+        float torque = 0;
+        if (RPM < idleRPM + 200 && FuelInput == 0 && currentGear == 0)
+        {
+            gearState = GearState.Neutral;
+        }
+        if (gearState == GearState.Running && clutch > 0)
+        {
+            if (RPM > increaseGearRPM)
+            {
+                StartCoroutine(ChangeGear(1));
+            }
+            else if (RPM < decreaseGearRPM)
+            {
+                StartCoroutine(ChangeGear(-1));
+            }
+        }
+        if (isEngineRunning > 0)
+        {
+            if (clutch < 0.1f)
+            {
+                RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM, redLine * FuelInput) + Random.Range(-50, 50), Time.deltaTime);
+            }
+            else
+            {
+                wheelRPM = Mathf.Abs((RRWheelCollider.rpm + RLWheelCollider.rpm) / 2f) * gearRatios[currentGear] * differentialRatio;
+                RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM - 100, wheelRPM), Time.deltaTime * 3f);
+                torque = (hpToRPMCurve.Evaluate(RPM / redLine) * MotorPower / RPM) * gearRatios[currentGear] * differentialRatio * 5252f * clutch;
+            }
+        }
+        return torque;
     }
 
     //Steering Method
     void ApplySteering(){
         FLWheelCollider.steerAngle = SteeringInput * SteeringPower;
         FRWheelCollider.steerAngle = SteeringInput * SteeringPower;
+    }
+
+    void CheckParticles()
+    {
+        WheelHit[] wheelHits = new WheelHit[4];
+        FRWheelCollider.GetGroundHit(out wheelHits[0]);
+        FLWheelCollider.GetGroundHit(out wheelHits[1]);
+        RRWheelCollider.GetGroundHit(out wheelHits[2]);
+        RLWheelCollider.GetGroundHit(out wheelHits[3]);
+
+        float slipAllowance = 0.2f;
+
+        if ((Mathf.Abs(wheelHits[0].sidewaysSlip) + Mathf.Abs(wheelHits[0].forwardSlip) > slipAllowance))
+        {
+            // Play particle effect for FR wheel
+        }
+        else
+        {
+            // Stop particle effect for FR wheel
+        }
+
+        if ((Mathf.Abs(wheelHits[1].sidewaysSlip) + Mathf.Abs(wheelHits[1].forwardSlip) > slipAllowance))
+        {
+            // Play particle effect for FL wheel
+        }
+        else
+        {
+            // Stop particle effect for FL wheel
+        }
+
+        if ((Mathf.Abs(wheelHits[2].sidewaysSlip) + Mathf.Abs(wheelHits[2].forwardSlip) > slipAllowance))
+        {
+            // Play particle effect for RR wheel
+        }
+        else
+        {
+            // Stop particle effect for RR wheel
+        }
+
+        if ((Mathf.Abs(wheelHits[3].sidewaysSlip) + Mathf.Abs(wheelHits[3].forwardSlip) > slipAllowance))
+        {
+            // Play particle effect for RL wheel
+        }
+        else
+        {
+            // Stop particle effect for RL wheel
+        }
     }
 
     //Wheel Update Method
@@ -83,11 +257,67 @@ public class CarManager : MonoBehaviour
         Mesh.transform.rotation = quar;
     }
 
-    void ApplyBrakes(){
+    public float GetSpeedRatio()
+    {
+        var gas = Mathf.Clamp(Mathf.Abs(FuelInput), 0.5f, 1f);
+        return RPM * gas / redLine;
+    }
+    IEnumerator ChangeGear(int gearChange)
+    {
+        gearState = GearState.CheckingChange;
+        if (currentGear + gearChange >= 0)
+        {
+            if (gearChange > 0)
+            {
+                //increase the gear
+                yield return new WaitForSeconds(0.7f);
+                if (RPM < increaseGearRPM || currentGear >= gearRatios.Length - 1)
+                {
+                    gearState = GearState.Running;
+                    yield break;
+                }
+            }
+            if (gearChange < 0)
+            {
+                //decrease the gear
+                yield return new WaitForSeconds(0.1f);
+
+                if (RPM > decreaseGearRPM || currentGear <= 0)
+                {
+                    gearState = GearState.Running;
+                    yield break;
+                }
+            }
+            gearState = GearState.Changing;
+            yield return new WaitForSeconds(changeGearTime);
+            currentGear += gearChange;
+        }
+
+        if (gearState != GearState.Neutral)
+            gearState = GearState.Running;
+    }
+
+
+void ApplyBrakes(){
         FLWheelCollider.brakeTorque = BrakeInput * BrakePower*.7f;
         FRWheelCollider.brakeTorque = BrakeInput * BrakePower*.7f;
         RLWheelCollider.brakeTorque = BrakeInput * BrakePower*.3f;
         RRWheelCollider.brakeTorque = BrakeInput * BrakePower*.3f;
     }
+
+}
+
+[System.Serializable]
+public class WheelParticles
+{
+    public ParticleSystem FRWheel;
+    public ParticleSystem FLWheel;
+    public ParticleSystem RRWheel;
+    public ParticleSystem RLWheel;
+
+    public TrailRenderer FRWheelTrail;
+    public TrailRenderer FLWheelTrail;
+    public TrailRenderer RRWheelTrail;
+    public TrailRenderer RLWheelTrail;
 
 }
---
+++ Assets/script/EngineAudio.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineAudio : MonoBehaviour
+{
+    public AudioSource runningSound;
+    public float runningMaxVolume;
+    public float runningMaxPitch;
+    public AudioSource reverseSound;
+    public float reverseMaxVolume;
+    public float reverseMaxPitch;
+    public AudioSource idleSound;
+    public float idleMaxVolume;
+    public float speedRatio;
+    private float revLimiter;
+    public float LimiterSound = 1f;
+    public float LimiterFrequency = 3f;
+    public float LimiterEngage = 0.8f;
+    public bool isEngineRunning = false;
+
+    public AudioSource startingSound;
+
+
+    private CarManager carManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carManager = GetComponent<CarManager>();
+        idleSound.volume = 0;
+        runningSound.volume = 0;
+        reverseSound.volume = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedSign = 0;
+        if (carManager)
+        {
+            speedSign = Mathf.Sign(carManager.GetSpeedRatio());
+            speedRatio = Mathf.Abs(carManager.GetSpeedRatio());
+        }
+        if (speedRatio > LimiterEngage)
+        {
+            revLimiter = (Mathf.Sin(Time.time * LimiterFrequency) + 1f) * LimiterSound * (speedRatio - LimiterEngage);
+        }
+        if (isEngineRunning)
+        {
+            idleSound.volume = Mathf.Lerp(0.1f, idleMaxVolume, speedRatio);
+            if (speedSign > 0)
+            {
+                reverseSound.volume = 0;
+                runningSound.volume = Mathf.Lerp(0.3f, runningMaxVolume, speedRatio);
+                runningSound.pitch = Mathf.Lerp(0.3f, runningMaxPitch, speedRatio);
+            }
+            else
+            {
+                runningSound.volume = 0;
+                reverseSound.volume = Mathf.Lerp(0f, reverseMaxVolume, speedRatio);
+                reverseSound.pitch = Mathf.Lerp(0.2f, reverseMaxPitch, speedRatio);
+            }
+        }
+        else
+        {
+            idleSound.volume = 0;
+            runningSound.volume = 0;
+        }
+    }
+    public IEnumerator StartEngine()
+    {
+        startingSound.Play();
+        carManager.isEngineRunning = 1;
+        yield return new WaitForSeconds(0.6f);
+        isEngineRunning = true;
+        yield return new WaitForSeconds(0.4f);
+        carManager.isEngineRunning = 2;
+    }
+}
