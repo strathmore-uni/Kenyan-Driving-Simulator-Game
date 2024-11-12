012c93ff5020177684b40922fe1e22d0f2057da9
Munziu <annkanyanga@gmail.com>
2024-11-12 17:16:46 +0300

    Add settings dialog

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -11,7 +11,54 @@ public class MenuController : MonoBehaviour
     public Slider volumeSlider = null;
     public float defaultVolume = 1.0f;
 
+    public TMP_Text controllerSenTextValue = null;
+    public Slider controllerSenSlider = null;
+    public int defaultSen = 4;
+    public int mainControllerSen = 4;
+
+    public Slider brightnessSlider = null;
+    public TMP_Text brightnessTextValue = null;
+    public float defaultBrightness = 1;
+
+    public TMP_Dropdown qualityDropdown; // Fixed variable name
+    public Toggle fullScreenToggle; // Added Toggle for fullscreen
+
+    private int _qualityLevel;
+    private bool _isFullScreen;
+    private float _brightnessLevel;
+
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
     public GameObject confirmationPrompt = null;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void ExitButton()
     {
@@ -29,7 +76,48 @@ public class MenuController : MonoBehaviour
         PlayerPrefs.SetFloat("mastervolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox());
     }
-    
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        _brightnessLevel = brightness;
+        brightnessTextValue.text = brightness.ToString("0.0");
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        _isFullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
+
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0));
+        Screen.fullScreen = _isFullScreen;
+
+        StartCoroutine(ConfirmationBox());
+    }
+
     public void ResetButton(string MenuType)
     {
         if (MenuType == "Audio")
@@ -38,6 +126,27 @@ public class MenuController : MonoBehaviour
             volumeSlider.value = defaultVolume;
             volumeTextValue.text = defaultVolume.ToString("0.0");
             VolumeApply();
+        }
+        if (MenuType == "Gameplay")
+        {
+            controllerSenTextValue.text = defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            GameplayApply();
+        }
+        if (MenuType == "Graphics")
+        {
+            brightnessTextValue.text = defaultBrightness.ToString("0.0");
+            brightnessSlider.value = defaultBrightness;
+
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1); // Fixed typo
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            resolutionDropdown.value = resolutionDropdown.options.Count - 1; // Set to the last resolution option
+            GraphicsApply();
         }
     }
 
